fbffed8190d94a3cb0a88f706a9e6eb52da666e3
Rasmus Nielsen <rasmus@easify.dk>
2021-09-16 23:29:38 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref} from "vue"
+import DeveloperPreview from "./components/DeveloperPreview.vue"
+
+const developerId = ref(1)
+</script>
+
+<template>
+    <div class="bg-white border-b border-gray-100 py-6 text-center">
+        <div class="inline-flex items-center space-x-4">
+            <label for="developerNo">Search developer #</label>
+            <input id="developerNo" type="number" class="rounded-lg py-2 px-4 bg-blue-50 focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Type number" v-model="developerId">
+        </div>
+    </div>
+
+    <div class="mt-4 w-full flex items-center justify-center">
+        <DeveloperPreview :id="developerId" />
+    </div>
+</template>
---
+++ src/components/DeveloperPreview.vue
@@ -0,0 +1,42 @@
+<script setup>
+import {computed, ref} from 'vue'
+import developers from '../developers'
+
+const props = defineProps({
+    id: Number
+})
+
+const layers = computed(() => {
+    const developer = developers.find(dev => parseInt(dev.id) === parseInt(props.id))
+
+    const os = 'https://user-images.githubusercontent.com/51840990/133480481-16adbf7c-7c3d-4e35-aa9a-31e354564681.png';
+    const computer = 'https://user-images.githubusercontent.com/51840990/133295766-b126eda3-d798-4dd0-89f1-825a52c574d1.png';
+    const flag = 'https://user-images.githubusercontent.com/51840990/133310175-2a2eb58f-be74-44f8-9d9d-2ef8fcf0f82f.png'
+    const background = 'https://user-images.githubusercontent.com/51840990/133120337-84e6a624-4f58-42ba-8733-1ac9d4b39d71.png'
+
+    return developer
+        ? [background, os, computer, flag]
+        : null
+})
+
+const count = ref(0)
+</script>
+
+<template>
+    <div class="text-center w-full max-w-md">
+        <h1 class="font-bold text-lg">Developer #{{ props.id }}</h1>
+
+        <div class="relative w-full" v-if="layers">
+            <img
+                v-for="layer in layers"
+                class="absolute top-0 left-0 w-full"
+                :key="layer"
+                :src="layer"
+            />
+        </div>
+
+        <div class="mt-4" v-else>
+            NOT FOUND
+        </div>
+    </div>
+</template>
